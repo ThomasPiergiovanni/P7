960e5831a813fdf0b86ada1eefdd4d73020c5050
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-06 13:02:52 +0100

    renaming files and folder

--- grandpy/form.py
+++ grandpy/form.py
@@ -3,6 +3,8 @@ form module
 """
 
 class Form():
+    """
+    """
     def __init__(self):
         self.question_label = "Ask your question here: \n"
         self.question = None
@@ -10,9 +12,13 @@ class Form():
         self.random_info_answer = None
 
     def ask_question(self):
+        """
+        """
         self.question = input(self.question_label)
 
     def see_question(self):
+        """
+        """
         print("Question asked by user: ",self.question)
 
 
