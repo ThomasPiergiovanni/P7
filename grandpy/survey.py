d27339160ab3edd391077b9a85d75039201aba9c
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-11 17:27:33 +0100

    progress made on parser

--- grandpy/survey.py
+++ grandpy/survey.py
@@ -85,22 +85,30 @@ class Survey:
         end_2 = search(r"\b1", reversed_pattern)
         end_3 = search(r"\B1", reversed_pattern)
         start_position = None
+        end_position = None
+        valid_word_present = True
 
         if start_1:
-            print("start_1:", start_1.start())
+            start_position = start_1.start()
         elif start_2:
-            print("start_2:", start_2.start())
+            start_position =  start_2.start()
         else:
-            print("No stop word")
+            valid_word_present = False
 
         if end_1:
-            print("end_1:", end_1.start() -1)
+            end_position = end_1.start() - 1
         elif end_2:
-            print("end_2:", pattern_len - end_2.start())
+            end_position = pattern_len - end_2.start()
         elif end_3:
-            print("end_3:", pattern_len - end_3.start())
+            end_position = pattern_len - end_3.start()
         else:
-            print("No stop word")
+            valid_word_present = False
+
+        if valid_word_present:
+            print ("Starts at index:",start_position,
+            "\nEnds at index:", end_position )
+        else:
+            print("No word is valid")
 
     def regular_expression_end(self):
         pattern = "001101"
