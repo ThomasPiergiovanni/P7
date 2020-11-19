2f26dcbb21f0dff832f44059fb9a0ed6aa8bdb26
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-19 22:37:07 +0100

    form submit is ok

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app #j importe ici app, va variable de classe Flask qui est dans __init__
 from app.forms import LoginForm
 
 @app.route('/') 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 #decorateur qui modifie la fonction route. Ici, si le navigateur demande / ou / index,
 #dans tous les cas il retournera le return de la fonvtion index. pkoi index?
 # parce que c est la seule view disponible.
 
 def index():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash("Question has been asked!")
+        return redirect('/index')
     return render_template("index.html", form=form)
+
     
