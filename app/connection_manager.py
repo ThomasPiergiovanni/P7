599af6c40862a490648d198b6eb8d7549296516e
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-20 19:19:34 +0100

    minor refactoring

--- app/connection_manager.py
+++ app/connection_manager.py
@@ -17,7 +17,7 @@ class ConnectionManager:
         self.locationbias = "rectangle:48.7783,2.3129|48.7801,2.3168"
 
         self.parameters = {
-                "input" : "cave nysa",
+                "input" : self.input,
                 "inputtype" : "textquery",
                 "fields": "name,formatted_address,geometry,types",
                 "locationbias": "rectangle:48.7783,2.3129|48.7801,2.3168",
@@ -29,8 +29,6 @@ class ConnectionManager:
     def get_places(self):
 
         try:
-            # response_api = requests.get(
-            #     self.url_1)
             response_api = requests.get(self.endpoint, params = self.parameters)
             self.places_api_answer = response_api.json()
             print(self.places_api_answer)
