8e3a845a5c0888cb75569377428d993bee0cacde
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-23 15:45:12 +0100

    getting answer from GG API displayed in website

--- grandpy/apiclients/google_places.py
+++ grandpy/apiclients/google_places.py
@@ -8,16 +8,16 @@ from configuration.config import Config
 class GooglePlaces:
     """
     """
-    def __init__(self):
+    def __init__(self, parsed_string):
         self.config = Config()
         self.endpoint = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
-        self.input = "les caves nysa"
-        self.inputtype = "textquery"
-        self.fields = "formatted_address,name"
-        self.locationbias = "rectangle:48.7783,2.3129|48.7801,2.3168"
+        self.input = None
+        # self.inputtype = "textquery"
+        # self.fields = "formatted_address,name"
+        # self.locationbias = "rectangle:48.7783,2.3129|48.7801,2.3168"
 
         self.parameters = {
-                "input" : self.input,
+                "input" : parsed_string,
                 "inputtype" : "textquery",
                 "fields": "name,formatted_address,geometry,types",
                 "locationbias": "rectangle:48.7783,2.3129|48.7801,2.3168",
@@ -25,6 +25,7 @@ class GooglePlaces:
                 }
 
         self.places_api_answer = None
+        self.address = ""
 
     def get_places(self):
 
@@ -44,27 +45,28 @@ class GooglePlaces:
                 " tard ou contacter le propriétaire de l'application")
 
     def get_attribute(self):
-        response = {'candidates': 
-                        [
-                            {'formatted_address': '1 bis Rue René Roeckel, 92340 Bourg-la-Reine, France',
-                            'geometry': {'location': {'lat': 48.779397, 'lng': 2.3149264}, 
-                                        'viewport': {'northeast': {'lat': 48.78074967989272, 'lng': 2.316450729892722},
-                                                    'southwest': {'lat': 48.77805002010727, 'lng': 2.313751070107277}
-                                                    }},
-                            'name': 'Les Caves Nysa',
-                            'types': ['liquor_store', 'food', 'point_of_interest', 'store', 'establishment']
-                            }
-                        ],
-                    'status': 'OK'}
+        # response = {'candidates': 
+        #                 [
+        #                     {'formatted_address': '1 bis Rue René Roeckel, 92340 Bourg-la-Reine, France',
+        #                     'geometry': {'location': {'lat': 48.779397, 'lng': 2.3149264}, 
+        #                                 'viewport': {'northeast': {'lat': 48.78074967989272, 'lng': 2.316450729892722},
+        #                                             'southwest': {'lat': 48.77805002010727, 'lng': 2.313751070107277}
+        #                                             }},
+        #                     'name': 'Les Caves Nysa',
+        #                     'types': ['liquor_store', 'food', 'point_of_interest', 'store', 'establishment']
+        #                     }
+        #                 ],
+        #             'status': 'OK'}
 
-        candidates = response["candidates"]
+
+        candidates = self.places_api_answer["candidates"]
         for candidate in candidates:
             name = candidate["name"]
-            address = candidate["formatted_address"]
+            self.address = candidate["formatted_address"]
             latitude = candidate["geometry"]["location"]["lat"]
             longitude = candidate["geometry"]["location"]["lng"]
             print(name)
-            print(address)
+            print(self.address)
             print(latitude)
             print(longitude)
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, flash, redirect, url_for
+
+from grandpy.apiclients.google_places import GooglePlaces
+from grandpy.parser.parser import Parser
 from website import app #j importe ici app, va variable de classe Flask qui est dans __init__
 from website.forms import LoginForm
 
-from grandpy.parser.parser import Parser
+
 
 
 @app.route('/', methods=['GET', 'POST']) 
@@ -15,9 +18,14 @@ def index():
     form = LoginForm()
     if form.validate_on_submit():
         parser = Parser(form.question.data)
+        flash("Your question : " + form.question.data)
+        flash("The parsed string : " + parser.parsed_string)
+        google_places_api = GooglePlaces(parser.parsed_string)
+        google_places_api.get_places()
+        google_places_api.get_attribute()
+        flash("GranPy answer : " + google_places_api.address)
 
-        flash(parser.parsed_string)
+
 
         return redirect(url_for("index")) # cela ramene a index, donc le formulaire sera vié puisque nvelle instance
     return render_template('index.html', form=form)
-  
