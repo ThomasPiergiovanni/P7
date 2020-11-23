7c3ec18cfb61f81899497ffe96f2f54a37d050d2
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-11-23 14:18:15 +0100

    renaming website module routes into views

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,6 @@ app.config.from_object(Config)
 # Je defini mon attribut config grace a lacd ap methode from object.
 # qui va recupérer la classe de configuration Config
 
-from website import routes 
-# On importe le module routes dans le package app (en non pas
-# la variuable). On le place  à la fin pour eviter 
+from website import views 
+# On importe le module views . On le place  à la fin pour eviter 
 # les import circulaire.
