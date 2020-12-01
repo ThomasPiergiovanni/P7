fc44e2719f28a5de26e4323e08c863a118258168
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-01 15:36:15 +0100

    ajax initial implementation. seems functionnal

--- website/views.py
+++ website/views.py
@@ -1,45 +1,44 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, request, jsonify, make_response
 
 from grandpy.apiclients.gmap import Gmap
 from grandpy.apiclients.place import Place
 from grandpy.apiclients.mediawiki import MediaWiki
 from grandpy.parser.parser import Parser
 from website import app 
-from website.form import Form
+# from website.form import Form
 
 
 @app.route('/', methods=['GET', 'POST']) 
 @app.route('/index', methods=['GET', 'POST'])
-
 def index():
     gmap = Gmap()
-    form = Form()
-    if form.validate_on_submit():
-        parser = Parser()
-        parser.question = form.question.data
-        parser.parse()
-        if parser.status:
-            flash("Ma question : " + form.question.data)
-            flash("Output parser: " + parser.parsed_string)
-            place = Place(parser.parsed_string)
-            place.get_place()
-            place.set_attribute()
-            if place.status:
-                mediawiki = MediaWiki(place.name)
-                mediawiki.get_mediawiki()
-                mediawiki.set_attribute()
-                if mediawiki.status:
-                    flash("L'adresse de: "
-                            + place.name
-                            + " est "
-                            + place.address)
-                    flash("Info wiki: " + mediawiki.information)
-                    gmap.set_place_location(place.place_id)
-                    return render_template('index.html', form=form, gmap=gmap)
-                else:
-                    flash("Mmh, pas d'info sur ce lieu")
-            else:
-                flash("Mmh, je ne connais pas cet endroit")
-        else:
-            flash("Mmh, je n'ai pas compris ce que tu me demandes")
-    return render_template('index.html', form=form, gmap=gmap)
+    return render_template('index.html', gmap=gmap)
+
+@app.route('/index/create-entry', methods=['POST'])
+def create_entry():
+    demand = request.get_json()
+
+    parser = Parser()
+    parser.question = demand["question"]
+    parser.parse()
+    if parser.status:
+        place = Place(parser.parsed_string)
+        place.get_place()
+        place.set_attribute()
+        if place.status:
+            mediawiki = MediaWiki(place.name)
+            mediawiki.get_mediawiki()
+            mediawiki.set_attribute()
+            if mediawiki.status:
+                gmap = Gmap()
+                gmap.set_place_location(place.place_id)
+
+    answer = {
+        "parsed_string" : parser.parsed_string,
+        "address" : place.address,
+        "information" : mediawiki.information,
+        "map": gmap.url
+    }
+    res = make_response(jsonify(answer), 200)
+    return res
+
