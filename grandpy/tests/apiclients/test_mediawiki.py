5db9af5618cb32b1bb1f943f0c89ebc0cbf9b238
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-11 20:07:11 +0100

    refactoring test mediawiki

--- grandpy/tests/apiclients/test_mediawiki.py
+++ grandpy/tests/apiclients/test_mediawiki.py
@@ -3,89 +3,128 @@
 from grandpy.apiclients.mediawiki import MediaWiki
 
 
-class TestMediaWiki:
-    def test_get_mediawiki_when_exists(self, monkeypatch):
-        place_ok = "Bourg-la-Reine"
-        response_ok = {
-                'batchcomplete': '',
-                'query': {
-                    'pages': {
-                        '80897': {
-                            'pageid': 80897,
-                            'ns': 0,
-                            'title': 'Bourg-la-Reine',
-                            'extract': "Bourg-la-Reine est une commune française du département des Hauts-de-Seine en région Île-de-France, dans l'arrondissement d'Antony, au sud de Paris. \nElle fait partie de la métropole du Grand Paris créée en 2016.\n\n"
+def test_get_mediawiki_with_an_existing_place(monkeypatch):
+    """Method that test get_mediawiki method by providing a valid place
+    """
+    place_ok = "Bourg-la-Reine"
+    response_ok = {
+            'batchcomplete': '',
+            'query': {
+                'pages': {
+                    '80897': {
+                        'pageid': 80897,
+                        'ns': 0,
+                        'title': 'Bourg-la-Reine',
+                        'extract': (
+                                "Bourg-la-Reine est une commune"
+                                "française du département des"
+                                " Hauts-de-Seine en région Île-de-France,"
+                                " dans l'arrondissement d'Antony, au sud "
+                                "de Paris. \nElle fait partie de la "
+                                "métropole du Grand Paris créée "
+                                "en 2016.\n\n")}}}}
+
+    class MockResponse:
+        """MockResponse class
+        """
+        def __init__(self, endpoint, params=None):
+            self.endpoint = endpoint
+            self.params = params
+            self.response = None
+
+        def json(self):
+            """Method returning a json object
+            """
+            self.response = response_ok
+            return self.response
+    monkeypatch.setattr("requests.get", MockResponse)
+    mediawiki = MediaWiki(place_ok)
+    mediawiki.get_mediawiki()
+    assert mediawiki.response == response_ok
+
+
+def test_get_mediawiki_with_a_not_valid_place(monkeypatch):
+    """Function that test get_mediawiki method by providing a not
+    valid place.
+    """
+    place_nok = "OpenClasdsdsrreoms"
+    response_nok = {
+            'batchcomplete': '',
+            'query': {
+                'pages': {
+                    '-1': {
+                        'ns': 0,
+                        'title': 'OpenClasdsdsrooms',
+                        'missing': ''
                         }}}}
-        class MockResponse:
-            def __init__(self, endpoint, params = None):
-                pass
 
-            def json(self):
-                return response_ok
-               
-        monkeypatch.setattr("requests.get", MockResponse)
-        mediawiki = MediaWiki(place_ok)
-        mediawiki.get_mediawiki()
-        assert mediawiki.response == response_ok
+    class MockResponse:
+        """MockResponse class
+        """
+        def __init__(self, endpoint, params=None):
+            self.endpoint = endpoint
+            self.params = params
+            self.response = None
+
+        def json(self):
+            """Method returning a json object
+            """
+            self.response = response_nok
+            return self.response
+    monkeypatch.setattr("requests.get", MockResponse)
+    mediawiki = MediaWiki(place_nok)
+    mediawiki.get_mediawiki()
+    assert mediawiki.response == response_nok
 
 
-    def test_get_mediawiki_when_does_not_exists(self, monkeypatch):
-        place_nok = "Brg-la-Rine"
-        response_nok = {
-                'batchcomplete': '',
-                'query':{
-                    'pages':{
-                        '-1': {
-                            'ns': 0,
-                            'title': 'OpenClasdsdsrooms',
-                            'missing': ''
-                            }}}}
-        class MockResponse:
-            def __init__(self, endpoint, params = None):
-                pass
+def test_set_attribute_with_exist():
+    """Function that test set_attribute method by providing a
+    valid place
+    """
+    place_ok = "Bourg-la-Reine"
+    response_ok = {
+            'batchcomplete': '',
+            'query': {
+                'pages': {
+                    '80897': {
+                        'pageid': 80897,
+                        'ns': 0,
+                        'title': 'Bourg-la-Reine',
+                        'extract': (
+                                "Bourg-la-Reine est une commune française"
+                                " du département des Hauts-de-Seine en"
+                                " région Île-de-France, dans"
+                                " l'arrondissement d'Antony, au sud de"
+                                " Paris. \nElle fait partie de la"
+                                " métropole du Grand Paris créée"
+                                " en 2016.\n\n")}}}}
+    mediawiki = MediaWiki(place_ok)
+    mediawiki.response = response_ok
+    mediawiki.set_attribute()
+    assert mediawiki.status is True
+    assert mediawiki.information == (
+            "Bourg-la-Reine est une commune française du département"
+            " des Hauts-de-Seine en région Île-de-France, dans"
+            " l'arrondissement d'Antony, au sud de Paris. \nElle fait"
+            " partie de la métropole du Grand Paris créée en 2016.\n\n")
 
-            def json(self):
-                return response_nok
-               
-        monkeypatch.setattr("requests.get", MockResponse)
-        mediawiki = MediaWiki(place_nok)
-        mediawiki.get_mediawiki()
-        assert mediawiki.response == response_nok
 
-    def test_set_attribute_when_exist(self):
-
-        place_ok = "Bourg-la-Reine"
-        response_ok = {
-                'batchcomplete': '',
-                'query': {
-                    'pages': {
-                        '80897': {
-                            'pageid': 80897,
-                            'ns': 0,
-                            'title': 'Bourg-la-Reine',
-                            'extract': "Bourg-la-Reine est une commune française du département des Hauts-de-Seine en région Île-de-France, dans l'arrondissement d'Antony, au sud de Paris. \nElle fait partie de la métropole du Grand Paris créée en 2016.\n\n"
+def test_set_attribute_when_does_not_exist():
+    """Method that test set_attribute method by providing a not
+    valid place
+    """
+    place_nok = "Brg-la-Rine"
+    response_nok = {
+            'batchcomplete': '',
+            'query': {
+                'pages': {
+                    '-1': {
+                        'ns': 0,
+                        'title': 'OpenClasdsdsrooms',
+                        'missing': ''
                         }}}}
-        mediawiki = MediaWiki(place_ok)
-        mediawiki.response = response_ok
-        mediawiki.set_attribute()
-        assert mediawiki.status == True
-        assert mediawiki.information == "Bourg-la-Reine est une commune française du département des Hauts-de-Seine en région Île-de-France, dans l'arrondissement d'Antony, au sud de Paris. \nElle fait partie de la métropole du Grand Paris créée en 2016.\n\n"
-
-    def test_set_attribute_when_does_not_exist(self):
-
-        place_nok = "Brg-la-Rine"
-        response_nok = {
-                'batchcomplete': '',
-                'query':{
-                    'pages':{
-                        '-1': {
-                            'ns': 0,
-                            'title': 'OpenClasdsdsrooms',
-                            'missing': ''
-                            }}}}
-        mediawiki = MediaWiki(place_nok)
-        mediawiki.response = response_nok
-        mediawiki.set_attribute()
-        assert mediawiki.status == False
-        assert mediawiki.information == None
-
+    mediawiki = MediaWiki(place_nok)
+    mediawiki.response = response_nok
+    mediawiki.set_attribute()
+    assert mediawiki.status is not True
+    assert mediawiki.information is None
