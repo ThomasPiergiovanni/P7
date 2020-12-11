9913bdd591a6a521c66105205b202cbeca2d9da9
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-11 18:52:02 +0100

    refactoring test_gmap done

--- grandpy/tests/apiclients/test_gmap.py
+++ grandpy/tests/apiclients/test_gmap.py
@@ -1,17 +1,19 @@
-"""Test mediawiki module
+"""Test Gmap module
 """
 from configuration.env import Env
 from grandpy.apiclients.gmap import Gmap
 
 
 class TestGmap:
+    """ Test Gmap class
     """
-    """
-    def test_set_place_location_when_placeid_is_provided(self):
-        """
+    def test_set_place_location_with_place_id_provided(self):
+        """Method that test set_place_location method with providing
+        a place id
         """
         place_id = "ChIJLU7jZClu5kcR4PcOOO6p3I0"
         gmap = Gmap()
         gmap.set_place_location(place_id)
-        assert gmap.url == "https://www.google.com/maps/embed/v1/place?key=" +\
-                Env.GG_API_KEY + "&q=place_id:" + place_id
+        assert gmap.url == (
+                "https://www.google.com/maps/embed/v1/place?key=" +
+                Env.GG_API_KEY + "&q=place_id:" + place_id)
