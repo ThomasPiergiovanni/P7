3413d70060c970f03a1e1627aaf98da76cf83c5b
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-11 16:01:40 +0100

    refactorring mediawiki ongoing

--- grandpy/apiclients/mediawiki.py
+++ grandpy/apiclients/mediawiki.py
@@ -1,8 +1,10 @@
+"""MediaWiki module.
+"""
 import requests
 
 
 class MediaWiki:
-    """
+    """ MediaWiki class
     """
     def __init__(self, parsed_string):
         self.endpoint = "https://fr.wikipedia.org/w/api.php"
@@ -20,7 +22,8 @@ class MediaWiki:
         self.status = False
 
     def get_mediawiki(self):
-        """
+        """Method that makes a parameterized request to Media WIki API and get
+        a response object back.
         """
         try:
             response_api = requests.get(self.endpoint, params = \
@@ -37,7 +40,8 @@ class MediaWiki:
                 " tard ou contacter le propriétaire de l'application")
 
     def set_attribute(self):
-        """
+        """Method that sets attributes values with informations
+        from the api response.
         """
         try:
             for key in self.response["query"]["pages"].keys():
