6cb13bec5934b9b610a9b55ebbc11266c3f38438
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-13 15:53:36 +0100

    making important change to html and css. seems ok but might be isue

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,10 @@
+"""Init module
+"""
 from flask import Flask
+
 from configuration.env import Env
 
 app = Flask(__name__)
-# Crée une instance de la classe Flask.
-
 app.config.from_object(Env)
-# Je defini mon attribut config grace a lacd ap methode from object.
-# qui va recupérer la classe de configuration Config
 
-from website import views 
-# On importe le module views . On le place  à la fin pour eviter 
-# les import circulaire.
+from website import views
--- website/views.py
+++ website/views.py
@@ -1,26 +1,33 @@
+"""Views module
+"""
 from flask import render_template, request, jsonify, make_response
 
 from grandpy.apiclients.gmap import Gmap
 from grandpy.apiclients.place import Place
 from grandpy.apiclients.mediawiki import MediaWiki
 from grandpy.parser.parser import Parser
-from website import app 
+from website import app
 
 
-@app.route('/', methods=['GET', 'POST']) 
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    """Index view
+    """
     gmap = Gmap()
     gmap.set_default_location()
     return render_template('index.html', gmap=gmap)
 
+
 @app.route('/index/create-entry', methods=['POST'])
 def create_entry():
+    """View when question is submitted
+    """
     demand = request.get_json()
     parser = Parser()
     parser.question = demand["question"]
     parser.parse()
-    place = Place(parser.parsed_string)
+    place = Place(parser.parsed_chain)
     place.get_place()
     place.set_attribute()
     mediawiki = MediaWiki(place.name)
@@ -33,14 +40,13 @@ def create_entry():
         gmap = Gmap()
         gmap.set_default_location()
     answer = {
-        "parser_status" : parser.status,
-        "parsed_string" : parser.parsed_string,
-        "address_status" : place.status,
-        "address" : place.address,
-        "information_status" : mediawiki.status,
-        "information" : mediawiki.information,
+        "parser_status": parser.status,
+        "parsed_chain": parser.parsed_chain,
+        "address_status": place.status,
+        "address": place.address,
+        "information_status": mediawiki.status,
+        "information": mediawiki.information,
         "map": gmap.url
     }
     response = make_response(jsonify(answer), 200)
     return response
-
