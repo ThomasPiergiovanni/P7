a01a8955b728dfbb5477e247f10a579d0adfc52d
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2020-12-16 19:42:22 +0100

    setting up secong gg api key. deployment

--- configuration/env.py
+++ configuration/env.py
@@ -20,4 +20,12 @@ class Env:
     # CUSTOM SETTINGS: You must replace the default key with your own
     # google api key. Fore more informations, please check "https://developers
     # .google.com/maps/gmp-get-started"
+    GG_API_KEY_RESTRICTED = os.environ.get("GG_API_KEY_BACK")
+
+    # DESCRIPTION: Google API key required for usage of google APIs.
+    # MANDATORY: Yes.
+    # DEFAULT SETTINGS: "xxxxxx"
+    # CUSTOM SETTINGS: You must replace the default key with your own
+    # google api key. Fore more informations, please check "https://developers
+    # .google.com/maps/gmp-get-started"
     GG_API_KEY = os.environ.get("GG_API_KEY")
